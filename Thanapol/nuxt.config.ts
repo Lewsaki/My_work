6dc7f9d3efc837dc419e30e9a09eacbe60ce2bc6
Lewsaki <0925156128zero@gmail.com>
2023-09-29 14:45:10 +0700

    Update nuxt.config.ts

--- Thanapol/nuxt.config.ts
+++ Thanapol/nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css' }]
+    }
+  },
+  ssr:false,
+  router:{
+    options:{
+      hashMode:false,
+    },
+  },
+  nitro:{
+    devProxy:{
+      '/api':{
+        target:'http://127.0.0.1:1880/api',
+        changeOrigin:true,
+      },
+    },
+  },
 })
